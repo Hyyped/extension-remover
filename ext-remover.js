f06699dd8d0e0022a4c52fdc7b5f8d9e74e277f2
Hyyped <110073191+Hyyped@users.noreply.github.com>
2023-02-03 08:35:26 -0500

    Update ext-remover.js

--- ext-remover.js
+++ ext-remover.js
@@ -1,5 +1,96 @@
-void function(){"chrome.google.com"===location.hostname%26%26location.pathname.startsWith("/webstore")%3Fchrome.management.getAll(a=>{function b(a){chrome.webstorePrivate.getExtensionStatus(a.id,b=>{if("force_installed"===b){const b=prompt(`What do you want to do with ${a.name}%3F (enable/disable)`);
-var c=""===a.homepageUrl%3F`<b><a title="${a.description}">${a.name}</a></b>`:`<b><a href="${a.homepageUrl}" title="${a.description}">${a.name}</a></b>`,d="";
-"disable"===b.toLowerCase()%3F(chrome.management.setEnabled(a.id,!1),d+=`<p>Disabled ${c}</p>`):"enable"===b.toLowerCase()%3F(chrome.management.setEnabled(a.id,!0),d+=`<p>Enabled ${c}</p>`):alert("Invalid option, try again!"),document.body.innerHTML+=d}})}document.body.innerHTML="",document.write("\n<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n<link href=\"https://fonts.googleapis.com/css2%3Ff...\" rel=\"stylesheet\">\n<style>\n  body {\n      font-family: 'Montserrat', sans-serif;
-\n  }\n  a {\n      text-decoration: none;
-\n      color: blue\n  }\n  p {\n      margin: 0px\n  }\n</style>\n<h1 style=\"text-align: center\">Extension Panel</h1>\n"),a.forEach(a=>b(a))}):(alert("Run this script again but while on this page"),location.href="https://chrome.google.com/webstore_")}();
+if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
+    location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
+}
+
+const style = document.createElement("style");
+document.head.replaceChildren(style);
+style.innerText = `
+
+body {
+  margin: 0;
+  background-color:#121212;
+}
+table {
+  width: 100%;
+}
+tr:nth-child(even) {
+  background-color: #2d2d2d;
+}
+tr:hover {
+  background-color: #ddd;
+}
+td {
+  text-align: center;
+  border: 1px solid #352e3f;
+  padding: 8px;
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  background-color: #1f1f1f;
+  color: white;
+}
+label {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 23px;
+}
+input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+span {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #8c8c8c;
+  transition: .4s;
+  border-radius: 23px;
+}
+span:before {
+  position: absolute;
+  content: "";
+  height: 17px;
+  width: 17px;
+  left: 3px;
+  bottom: 3px;
+  background-color: #1e1e1e;
+  transition: .4s;
+  border-radius: 50%;
+}
+input:checked + span {
+  background-color: #bb86fc;
+}
+input:focus + span {
+  box-shadow: 0 0 1px #2196F3;
+}
+input:checked + span:before {
+  transform: translateX(17px);
+}
+`;
+
+chrome.management.getAll(extensions => {
+    const table = document.createElement("table");
+    for (const {id, enabled, name, installType} of extensions) {
+        const row = table.appendChild(document.createElement("tr"));
+        const label = row
+            .appendChild(document.createElement("td"))
+            .appendChild(document.createElement("label"));
+
+        const input = label.appendChild(document.createElement("input"));
+        input.type = "checkbox";
+        input.checked = enabled;
+        input.addEventListener("change", () => {
+            chrome.management.setEnabled(id, input.checked);
+        });
+
+        label.appendChild(document.createElement("span"));
+        row.appendChild(document.createElement("td")).innerText = name;
+        row.appendChild(document.createElement("td")).innerText = id;
+        row.appendChild(document.createElement("td")).innerText = installType;
+    }
+    document.body.replaceChildren(table);
+});
